77f9037e22d4421ce82c5674602c9dab559b8b80
崔世豪 <virus_cui@outlook.com>
2023-02-11 15:11:00 +0800

    测试库

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import index from "@/views/index.vue";
+import login from "@/views/auth/login.vue";
+import test from "@/views/test.vue"
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'index',
     component: index
+  },
+  {
+    path: '/auth/login',
+    name: 'login',
+    component: login
+  },
+  {
+    path: '/test',
+    name: 'test',
+    component: test
   }
 ]
 
---
+++ src/views/auth/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="login-main">
+    <h1>Login</h1>
+  </div>
+</template>
+
+<script>
+
+import $ from 'jquery'
+
+export default {
+  name: "login",
+  mounted() {
+    $("title").text("登录")
+  }
+}
+</script>
+
+<style>
+
+</style>
