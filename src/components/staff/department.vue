2d0341cf354dd3147e8bcc9fdd89b446b389626d
瑾然 <2090515528@qq.com>
2023-02-12 01:08:58 +0800

    导航栏

--- src/components/staff/department.vue
+++ src/components/staff/department.vue
@@ -1,5 +1,25 @@
 <template>
     <h1>员工部门</h1>
+    <div class="container">
+        <div class="table">
+            <!--        <el-pagination-->
+            <!--                background-->
+            <!--                layout="prev, pager, next"-->
+            <!--                :total="1000">-->
+            <!--        </el-pagination>-->
+
+            <el-pagination
+                    layout="prev, pager, next"
+                    :page-sizes="[5, 10, 15, 20]"
+                    :page-size="pagesize"
+                    :total="count"
+                    @prev-click="prev"
+                    @current-change="change"
+                    @next-click="next"
+            >
+            </el-pagination>
+        </div>
+    </div>
 </template>
 
 <script>
